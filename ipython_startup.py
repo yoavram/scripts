3ef55e901822ec48fdf3f233f99255e68b565e2b
Yoav Ram <yoavram@gmail.com>
2014-02-03 13:55:06 +0200

    added styles

--- ipython_startup.py
+++ ipython_startup.py
@@ -3,3 +3,9 @@ import numpy as np
 from matplotlib.pyplot import *
 import scipy
 import pandas as pd
+
+np.set_printoptions(linewidth=80)
+np.set_printoptions(precision=3)
+
+from mpltools import style
+style.use('ggplot')
