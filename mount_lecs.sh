9f003eb1ab8bbc28604dae53b088cf1b3298d2cc
Yoav Ram <yoavram+github@gmail.com>
2014-10-05 15:34:08 +0300

    lecs2

--- mount_lecs.sh
+++ mount_lecs.sh
@@ -4,4 +4,4 @@
 #http://www.debuntu.org/2006/04/27/39-mounting-a-fuse-filesystem-form-etcfstab
 #http://sshkeychain.sourceforge.net/mirrors/SSH-with-Keys-HOWTO/SSH-with-Keys-HOWTO-4.html
 
-sshfs yoavram@lecs.tau.ac.il: ~/lecs/
+sshfs yoavram@lecs2.tau.ac.il: ~/lecs/
