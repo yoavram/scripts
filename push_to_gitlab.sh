405639badf7a38044c54eeb03f018cba60191f46
Yoav Ram <yoav@yoavram.com>
2017-01-08 11:29:38 +0200

    push to github, not just gitlab

--- push_to_gitlab.sh
+++ push_to_gitlab.sh
@@ -2,4 +2,5 @@
 # http://stackoverflow.com/questions/14290113/git-pushing-code-to-two-remotes
 REPO_PATH=$(git rev-parse --show-toplevel)
 REPO_NAME=$(basename $REPO_PATH)
+git remote set-url --add --push origin git@github.com:yoavram/$REPO_NAME.git
 git remote set-url --add --push origin git@gitlab.com:yoavram/$REPO_NAME.git
